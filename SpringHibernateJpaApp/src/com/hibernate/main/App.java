13bcdd1438ac5ebcceec1ec9e97dbf5356a1ab1e
Samiksha Patil <samikshapatil419@gmail.com>
2025-05-22 11:10:16 +0530

    Add files via upload

---
+++ SpringHibernateJpaApp/src/com/hibernate/main/App.java
@@ -0,0 +1,11 @@
+package com.hibernate.main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context 
+					= new AnnotationConfigApplicationContext(AppConfig.class);
+	}
+
+}
---
+++ SpringHibernateJpaApp/src/com/hibernate/main/AppConfig.java
@@ -0,0 +1,56 @@
+package com.hibernate.main;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages = "com.hibernate.main")
+@EnableTransactionManagement
+public class AppConfig {
+	
+	static {
+		System.out.println("AppConfig called....");
+	}
+	@Bean
+	public DataSource getDataSource() {
+		String url="jdbc:mysql://localhost:3306/lms_hibernate_db";
+		String userDB = "root";
+		String passDB = "techskillsit";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		DriverManagerDataSource dataSource = new DriverManagerDataSource(url, userDB, passDB);
+		dataSource.setDriverClassName(driver);
+		
+		return dataSource;
+	}
+	@Bean
+	public LocalContainerEntityManagerFactoryBean getEntityManagerFactory(){
+		LocalContainerEntityManagerFactoryBean emFactory
+						= new LocalContainerEntityManagerFactoryBean();
+		emFactory.setDataSource(getDataSource());
+		emFactory.setPackagesToScan("com.hibernate.main.model");
+		
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		emFactory.setJpaVendorAdapter(vendorAdapter);
+		emFactory.setJpaProperties(getProperties());
+		return emFactory;
+	}
+	
+	
+	public Properties getProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "update"); //create, create-drop,update,none
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+		return properties;
+	}
+}
+ 
+
